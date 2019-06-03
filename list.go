453049b333fe4d789bb81091d47be0edfa20b69e
Wédney Lima <wedney.lima@b2wdigital.com>
2019-06-03 13:31:18 -0300

    list: example simples list

---
+++ list.go
@@ -0,0 +1,6 @@
+package estruturadados
+
+// Contains reports whether value is within values.
+func Contains(values []string, value string) bool {
+	return true
+}
---
+++ list_test.go
@@ -0,0 +1,17 @@
+package estruturadados
+
+import "testing"
+
+func TestContains(t *testing.T) {
+	values := []string{"a", "b", "c", "d", "e"}
+
+	for _, v := range values {
+		if !Contains(values, v) {
+			t.Fatalf("Contains returned false for %v", v)
+		}
+	}
+
+	if !Contains(values, "z") {
+		t.Fatalf("Contains returned true for %v", "z")
+	}
+}
